03f8a9519cd2faf65e56d2d20d918baa69d9127f
Rahmat Lutvi Furkon <lutvi1500@gmail.com>
2022-06-20 08:31:29 +0700

    fix query for date

--- application/controllers/C_cetak.php
+++ application/controllers/C_cetak.php
@@ -1,97 +1,95 @@
 <?php
 
+defined('BASEPATH') or exit('No direct script access allowed');
 
-defined('BASEPATH') OR exit('No direct script access allowed');
+class C_cetak extends CI_Controller
+{
 
-class C_cetak extends CI_Controller {
-
-	public function __construct()
+    public function __construct()
     {
         parent::__construct();
         date_default_timezone_set('Asia/Jakarta');
     }
-	public function index()
-	{
-		
-	}
+    public function index()
+    {
 
-	public function printDataHargaBarang()
-	{
-		$data['title']='print data harga barang';
-		$data['dataHarga']=$this->model->getHarga();
-		$mpdf = new Mpdf\Mpdf(['format'=>'Legal']);
+    }
+
+    public function printDataHargaBarang()
+    {
+        $data['title'] = 'print data harga barang';
+        $data['dataHarga'] = $this->model->getHarga();
+        $mpdf = new Mpdf\Mpdf(['format' => 'Legal']);
         $mpdf->AddPage('L');
-        $cetak= $this->load->view('admin/v_print_data_harga',$data, true);
+        $cetak = $this->load->view('admin/v_print_data_harga', $data, true);
         $mpdf->WriteHtml($cetak);
         $mpdf->Output();
-	}
-	
-	public function tampilLaporanPerbulan()
-	{
-		// $vtanggal = $this->input->post('vtanggal');
-		// $data['title']='print data harga barang';
-		// $mpdf = new Mpdf\Mpdf(['format'=>'Legal']);
-		// $mpdf->AddPage('L');
-		// $data ['dataHarga']= $this->model->tampilLaporanPerbulan($vtanggal);
-		// $cetak = $this->load->view('admin/v_print_data_perbulan', $data, true);
-		// $mpdf->WriteHtml($cetak);
-		// $mpdf->Output();
-		$vtanggal=$this->input->post('vtanggal');
-		$data['title']='print data harga barang';
-		$data['dataHarga']=$this->model->tampilLaporanPerbulan($vtanggal);
-		$this->load->view('admin/v_print_data_pertanggal',$data); 
-		
-	}
-	
-	public function cetakPertanggal()
-	{
-		// ob_start();
-		$bulan = $this->input->post('bulan');
-		$tahun = $this->input->post('tahun');
-		
-		$data['title']='print data harga pertanggal';		
-		$data['printPertanggal'] = $this->model->getHargaPertanggal($bulan,$tahun);
-		echo json_encode($data['printPertanggal']);
-		// $mpdf = new Mpdf\Mpdf(['format'=>'Legal']);
+    }
+
+    public function tampilLaporanPerbulan()
+    {
+        // $vtanggal = $this->input->post('vtanggal');
+        // $data['title']='print data harga barang';
+        // $mpdf = new Mpdf\Mpdf(['format'=>'Legal']);
         // $mpdf->AddPage('L');
-		// $cetak = $this->load->view('admin/v_print_data_pertanggal', $data, true);
-		// $mpdf->WriteHtml($cetak);
+        // $data ['dataHarga']= $this->model->tampilLaporanPerbulan($vtanggal);
+        // $cetak = $this->load->view('admin/v_print_data_perbulan', $data, true);
+        // $mpdf->WriteHtml($cetak);
         // $mpdf->Output();
-		
-	}
-	public function cetakPerbulan()
-	{
-		// ob_start();
-		$bulan = $this->input->post('bulan');
-		$tahun = $this->input->post('tahun');
-		
-		$data = [
-			'bulan'=>$bulan,
-			'tahun'=>$tahun,
-			// 'laporan'=>$this->model->getHargaPerbulan($bulan,$tahun),
-		];
-		$data['title']='print data harga pertanggal';		
-		$data['printPerbulan'] = $this->model->getHargaPerbulan($bulan,$tahun);
-		// echo json_encode($data);
-		$mpdf = new Mpdf\Mpdf(['format'=>'Legal']);
+        $vtanggal = $this->input->post('vtanggal');
+        $data['title'] = 'print data harga barang';
+        $data['dataHarga'] = $this->model->tampilLaporanPerbulan($vtanggal);
+        $this->load->view('admin/v_print_data_pertanggal', $data);
+
+    }
+
+    public function cetakPertanggal()
+    {
+        // ob_start();
+        $bulan = $this->input->post('bulan');
+        $tahun = $this->input->post('tahun');
+
+        $data['title'] = 'print data harga pertanggal';
+        $data['printPertanggal'] = $this->model->getHargaPertanggal($bulan, $tahun);
+        echo json_encode($data['printPertanggal']);
+        // $mpdf = new Mpdf\Mpdf(['format'=>'Legal']);
+        // $mpdf->AddPage('L');
+        // $cetak = $this->load->view('admin/v_print_data_pertanggal', $data, true);
+        // $mpdf->WriteHtml($cetak);
+        // $mpdf->Output();
+
+    }
+    public function cetakPerbulan()
+    {
+        // ob_start();
+        $bulan = $this->input->post('bulan');
+        $tahun = $this->input->post('tahun');
+        $bulan = strlen($bulan) == 1 ? '0' . $bulan : $bulan;
+        $date = $bulan . '-' . $tahun;
+        $data = [
+            'bulan' => $bulan,
+            'tahun' => $tahun,
+            'laporan' => $this->model->getHargaPerbulan($date),
+        ];
+        $data['title'] = 'print data harga pertanggal';
+        $data['printPerbulan'] = $this->model->getHargaPerbulan($date);
+        // echo json_encode($data);
+        $mpdf = new Mpdf\Mpdf(['format' => 'Legal']);
         $mpdf->AddPage('L');
-		$cetak = $this->load->view('admin/v_print_data_perbulan', $data, true);
-		$mpdf->WriteHtml($cetak);
+        $cetak = $this->load->view('admin/v_print_data_perbulan', $data, true);
+        $mpdf->WriteHtml($cetak);
         $mpdf->Output();
-		
-	}
-	
-	public function filter($id)
-	{
-		$data['dataHarga'] = $this->model->getHargaPertanggal($id);
-		// $data ['dataHarga'] = $this->db->get_where('tb_harga',['id_harga'=>$id])->result_array();
-		$this->load->view('admin/v_harga', $data);
-		
-	}
+
+    }
+
+    public function filter($id)
+    {
+        $data['dataHarga'] = $this->model->getHargaPertanggal($id);
+        // $data ['dataHarga'] = $this->db->get_where('tb_harga',['id_harga'=>$id])->result_array();
+        $this->load->view('admin/v_harga', $data);
+
+    }
 
 }
 
 /* End of file C_cetak.php */
-
-
-?>
--- application/models/Model.php
+++ application/models/Model.php
@@ -27,24 +27,24 @@ class Model extends CI_Model
         $this->db->order_by('id_barang', 'desc');
         return $this->db->get()->result_array();
     }
-	public function kodeBarangOtomatis()
-	{
-		$this->db->select('RIGHT(tb_barang.kode_barang,2) as kode_barang', FALSE);
-		$this->db->order_by('kode_barang', 'desc');
-		$this->db->limit(1);
-		$query = $this->db->get('tb_barang');
-		if ($query->num_rows() > 0) {
-			$data = $query->row();
-			$kode = intval($data->kode_barang)+1;
-		}else {
-			$kode = 1;
-		}
-		$tgl=date('dmY'); 
-        $batas = str_pad($kode, 3, "0", STR_PAD_LEFT);    
-        $kodetampil = "KD"."5".$tgl.$batas;  //format kode
-        return $kodetampil;  
-		
-	}
+    public function kodeBarangOtomatis()
+    {
+        $this->db->select('RIGHT(tb_barang.kode_barang,2) as kode_barang', false);
+        $this->db->order_by('kode_barang', 'desc');
+        $this->db->limit(1);
+        $query = $this->db->get('tb_barang');
+        if ($query->num_rows() > 0) {
+            $data = $query->row();
+            $kode = intval($data->kode_barang) + 1;
+        } else {
+            $kode = 1;
+        }
+        $tgl = date('dmY');
+        $batas = str_pad($kode, 3, "0", STR_PAD_LEFT);
+        $kodetampil = "KD" . "5" . $tgl . $batas; //format kode
+        return $kodetampil;
+
+    }
 
     public function addBarang($data)
     {
@@ -75,75 +75,72 @@ class Model extends CI_Model
 
     public function deleteHarga($id)
     {
-		$this->db->where('id_harga', $id);
+        $this->db->where('id_harga', $id);
         $this->db->delete('tb_harga');
     }
-	
-	public function deleteBarang($id)
-	{
-		$this->db->where('id_barang', $id);
-		$this->db->delete('tb_barang');		
-	}
-	
-	public function deleteKelolaHarga($id)
-	{
-		$this->db->where('id_histori', $id);
-		$this->db->delete('tb_histori');
-	}
 
-	public function checkBarang($id)
-	{
-		$this->db->select('id_barang');
-		$this->db->from('tb_harga');
-		$this->db->where('id_barang', $id);
-		return $this->db->get()->num_rows();
-		
-		
-		
-	}
+    public function deleteBarang($id)
+    {
+        $this->db->where('id_barang', $id);
+        $this->db->delete('tb_barang');
+    }
 
-	public function updatePassword($id,$data)
-	{
-		$this->db->where('id_user', $id);
-		$this->db->update('tb_user', $data);
-	}
+    public function deleteKelolaHarga($id)
+    {
+        $this->db->where('id_histori', $id);
+        $this->db->delete('tb_histori');
+    }
 
-	public function findData($table,$reference,$id)
-	{
-		$this->db->from($table);
-		$this->db->where($reference, $id);
-		
-		return $this->db->get()->row_array();
-		
-		
-	}
-	public function findDataBarang()
-	{
-		$dataBarang = $this->db->get('tb_barang');
-		if ($dataBarang->num_rows() > 0) {
-			return $dataBarang->num_rows();
-		}else {
-			return 0;
-		}
-	}
-	public function findDataUser()
-	{
-		$dataUser = $this->db->get('tb_user');
-		if ($dataUser->num_rows() > 0) {
-			return $dataUser->num_rows();
-		}else {
-			return 0;
-		}
-	}
-	public function findDataHargaBarang()
-	{
-		$dataHarga = $this->db->get('tb_harga');
-		if ($dataHarga->num_rows() > 0) {
-			return $dataHarga->num_rows();
-		}else {
-			return 0;
-		}
-	}
+    public function checkBarang($id)
+    {
+        $this->db->select('id_barang');
+        $this->db->from('tb_harga');
+        $this->db->where('id_barang', $id);
+        return $this->db->get()->num_rows();
+
+    }
+
+    public function updatePassword($id, $data)
+    {
+        $this->db->where('id_user', $id);
+        $this->db->update('tb_user', $data);
+    }
+
+    public function findData($table, $reference, $id)
+    {
+        $this->db->from($table);
+        $this->db->where($reference, $id);
+
+        return $this->db->get()->row_array();
+
+    }
+    public function findDataBarang()
+    {
+        $dataBarang = $this->db->get('tb_barang');
+        if ($dataBarang->num_rows() > 0) {
+            return $dataBarang->num_rows();
+        } else {
+            return 0;
+        }
+    }
+    public function findDataUser()
+    {
+        $dataUser = $this->db->get('tb_user');
+        if ($dataUser->num_rows() > 0) {
+            return $dataUser->num_rows();
+        } else {
+            return 0;
+        }
+    }
+    public function findDataHargaBarang()
+    {
+        $dataHarga = $this->db->get('tb_harga');
+        if ($dataHarga->num_rows() > 0) {
+            return $dataHarga->num_rows();
+        } else {
+            return 0;
+        }
+    }
 
     public function findDataHarga($table, $reference, $id)
     {
@@ -181,42 +178,42 @@ class Model extends CI_Model
 
     }
 
-	public function tampilLaporanPerbulan($vtanggal)
-	{
-		$vbulan = date('m',strtotime($vtanggal));
-		$this->db->select('*');
-		$this->db->from('tb_harga');
-		$this->db->where('month(tgl_input)', $vbulan);
-		$this->db->where('year(tgl_input)', $vtanggal);
-		// $this->db->order_by('id_harga', 'desc');
+    public function tampilLaporanPerbulan($vtanggal)
+    {
+        $vbulan = date('m', strtotime($vtanggal));
+        $this->db->select('*');
+        $this->db->from('tb_harga');
+        $this->db->where('month(tgl_input)', $vbulan);
+        $this->db->where('year(tgl_input)', $vtanggal);
+        // $this->db->order_by('id_harga', 'desc');
         // $this->db->join('tb_barang', 'tb_barang.id_barang = tb_harga.id_barang', 'left');
-		
-		return $this->db->get()->result_array();
-		
-	}
-	public function getHargaPertanggal($bulan,$tahun)
-	{		
-		// $vbulan = date('m',strtotime($id));
-		$this->db->select('*');
-		$this->db->from('tb_harga');
-		$this->db->join('tb_barang', 'tb_barang.id_barang = tb_harga.id_barang', 'left');
-		// $this->db->query("SELECT * FROM tb_harga WHERE tgl_input = '$bulan'");
-		$this->db->where('MONTH(tgl_input)',$bulan);
-		$this->db->where('YEAR(tgl_input)',$tahun);
-		// $this->db->where('id_harga', $id);
-		return $this->db->get()->result_array();
-	}
-	public function getHargaPerbulan($bulan,$tahun)
-	{		
-		$this->db->select('');
-		$this->db->from('tb_harga');
-		// $this->db->join('tb_barang', 'tb_barang.id_barang = tb_harga.id_barang', 'left');
-		// $this->db->query("SELECT * FROM tb_harga WHERE tgl_input = '$bulan'");
-		$this->db->where('MONTH(tgl_input)',$bulan);
-		$this->db->where('YEAR(tgl_input)',$tahun);
-		// $this->db->where('id_harga', $id);
-		return $this->db->get()->result_array();
-	}
+
+        return $this->db->get()->result_array();
+
+    }
+    public function getHargaPertanggal($bulan, $tahun)
+    {
+        // $vbulan = date('m',strtotime($id));
+        $this->db->select('*');
+        $this->db->from('tb_harga');
+        $this->db->join('tb_barang', 'tb_barang.id_barang = tb_harga.id_barang', 'left');
+        // $this->db->query("SELECT * FROM tb_harga WHERE tgl_input = '$bulan'");
+        $this->db->where('MONTH(tgl_input)', $bulan);
+        $this->db->where('YEAR(tgl_input)', $tahun);
+        // $this->db->where('id_harga', $id);
+        return $this->db->get()->result_array();
+    }
+    public function getHargaPerbulan($date)
+    {
+        $this->db->select('');
+        $this->db->from('tb_harga');
+        $this->db->join('tb_barang', 'tb_barang.id_barang = tb_harga.id_barang', 'left');
+        // $this->db->query("SELECT * FROM tb_harga WHERE tgl_input = '$bulan'");
+        $this->db->where('substr(tgl_input,4,7)', $date);
+
+        // $this->db->where('id_harga', $id);
+        return $this->db->get()->result_array();
+    }
 
 }
 
